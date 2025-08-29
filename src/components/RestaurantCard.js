865fb0e0bc86a6d30cf82fc0e016babebdb69b58
[Ayushi Chhangani] <[ayushichhangani2020@gmail.com]>
2025-08-29 09:18:06 +0530

    commit changes

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,19 +8,26 @@ const RestaurantCard = (props) => {
       name,
       avgRating,
       cuisines,
+      areaName,
       costForTwo,
       sla,
-    } = resData?.info || {};
+    } = resData?.info;
 
     return (
-        <div className="res-card">
+        <div className="px-4 py-2 m-2 w-60 rounded-xl
+             transition duration-200 ease-in-out
+            font-sans flex flex-col cursor-pointer hover:scale-95">
             
-            <img className="res-logo" alt="res-logo" src={CDN_URL+cloudinaryImageId}></img>
-            <h3>{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
-            <h4>{avgRating} stars</h4>
-            <h4>{costForTwo}</h4>
-            <h4>{sla?.deliveryTime} minutes</h4>
+            <img className="h-38 object-cover rounded-xl" alt="res-logo" src={CDN_URL+cloudinaryImageId}></img>
+            <div className="py-2.5 px-3">
+            <h3 className="font-bold text-[rgba(2,6,12,0.92)] mb-[3px] py-2.5 font-sans text-[28px] leading-[22px]">{name}</h3>
+            <div className="flex justify-between text-[14px]">
+            <span className="bg-[#48c479] text-white px-1.5 py-0.5 rounded text-[14px] font-medium">{avgRating} ★</span>
+            <span className="text-[#686b78]">{sla?.deliveryTime} MINS</span>
+            </div>
+            <p className="text-[16px] text-[#686b78] mt-1.5 mb-[2px] h-9 overflow-hidden leading-[1.2] font-sans">{cuisines.join(", ")}</p>
+            <p className="text-[16px] text-[#686b78] mt-1.5 mb-[2px] h-9 overflow-hidden leading-[1.2] font-sans">{areaName}</p>
+            </div>
         </div>
     );
 };
